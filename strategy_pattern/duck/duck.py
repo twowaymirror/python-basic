a4b2fcb588c1dc0e1e5986b6cd92caa03e77ff59
Kenneth Karlsson <kenneth.karlsson.2@volvocars.com>
2021-11-07 20:30:34 +0100

    Add compeleted Duck strategy pattern,

--- strategy_pattern/duck/duck.py
+++ strategy_pattern/duck/duck.py
@@ -4,43 +4,74 @@ from abc import ABC, abstractclassmethod
 class FlyBehaviour(ABC):
     """Strategy FlyBehaviour"""
     @abstractclassmethod
-    def fly():
+    def fly(self):
         """Method for different fly behaviour"""
 
 class FlyWithWings(FlyBehaviour):
-    def fly():
-        print("Flying")
+    def fly(self):
+        return "Flying"
 
 class FlyNoWay(FlyBehaviour):
-    def fly():
-        print("Not lifting off!!!")
+    def fly(self):
+        return "Not lifting off!!!"
 
 
 class QuackBehaviour(ABC):
     """Strategy QuackBehaviour"""
     @abstractclassmethod
-    def quack():
+    def quack(self):
         """Method for different quack behaviour"""
 
 class Quack(QuackBehaviour):
-    def quack():
-        print("Quacking like a duck!!")
+    def quack(self):
+        return "Quacking like a duck!!"
 
 class Squeak(QuackBehaviour):
-    def quack():
-        print("Squeking like something that squeaks")
+    def quack(self):
+        return "Squeking like something that squeaks"
     
 class MuteQuack(QuackBehaviour):
-    def quack():
-        print("Silence")
+    def quack(self):
+        return "Silence"
 
-    
+
 class Duck():
-    """The Duck class with different properties depending on what duck getting generated"""
-    def __init__(self, quack: QuackBehaviour, fly: FlyBehaviour) -> None:
-        quack = QuackBehaviour
-        fly = FlyBehaviour
+    """ The Duck class is the Conctext
+        Generating different behaviour depending on strategy.
+        The Duck class is unaware of what behaviour that will be used, decoupling the Duck from
+        its behaviour.
+
+    """
+    def __init__(self, quack: QuackBehaviour, fly: FlyBehaviour) -> None:        
+        self.quack = quack
+        self.fly = fly
+
+    def swim(self):
+        print("Swimming")
+
+    def performFly(self):
+        print(self.quack.quack())
+
+    def performQuack(self):
+        print(self.fly.fly())
 
 
-if __name__ == "__manin__":
-    """Example of how to run the code, should probably be in a function/method in real life"""
+class MallardDuck(Duck):
+    """Create Mallard Duck"""
+    def __init__(self):
+        super().__init__(Quack(), FlyNoWay())
+
+
+if __name__ == "__main__":
+    """ 
+    Client
+    Example of how to run the code, should probably be in a function/method in real life
+    """
+    mallard = Duck(Squeak(), FlyNoWay())
+    mallard.performQuack()
+    mallard.performFly()
+    mallard.swim()
+
+    mallard2 = MallardDuck()
+    mallard2.performFly()
+    mallard2.performQuack()
